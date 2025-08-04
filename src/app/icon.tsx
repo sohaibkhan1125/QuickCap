d114ea22ea4ac25a6df9e71ce3d7f245411f7e27
Sohaib Faisal Khan <sohaibniazi171@gmail.com>
2025-08-04 11:05:08 +0000

    still blue box is showing in favicon. fix it and change it

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,4 +1,3 @@
-import { WavesIcon } from 'lucide-react';
 import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
@@ -14,7 +13,6 @@ export default function Icon() {
     (
       <div
         style={{
-          fontSize: 24,
           background: 'hsl(222 47% 11%)',
           width: '100%',
           height: '100%',
@@ -24,7 +22,21 @@ export default function Icon() {
           borderRadius: '6px',
         }}
       >
-        <WavesIcon color='hsl(222 100% 68%)' />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="hsl(222 100% 68%)"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M2 6c.6.5 1.2 1 2.5 1C7 7 7 5 9.5 5c2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+          <path d="M2 12c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+          <path d="M2 18c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+        </svg>
       </div>
     ),
     {
