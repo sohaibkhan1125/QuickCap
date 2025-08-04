476b5e02776db9d55b8b827ba7540bd95d829e8a
Sohaib Faisal Khan <sohaibniazi171@gmail.com>
2025-08-04 11:02:19 +0000

    now use my website logo in my website favicon also

---
+++ src/app/icon.tsx
@@ -0,0 +1,38 @@
+import { WavesIcon } from 'lucide-react';
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'hsl(222 47% 11%)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '6px',
+        }}
+      >
+        <WavesIcon
+            style={{
+                color: 'hsl(222 100% 68%)',
+            }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
